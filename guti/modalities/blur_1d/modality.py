f8adebe4e323abb011fcf5d620b7228da36cef93
Raffi Hotter <raphael.hotter@gmail.com>
2025-10-26 05:07:26 +0000

    rename folder

--- guti/modalities/blur_1d/modality.py
+++ guti/modalities/blur_1d/modality.py
@@ -16,41 +16,27 @@ class Blur1D(ImagingModality):
     def __init__(self, params: Optional[Parameters] = None):
         """
         Relevant params:
-        - num_brain_grid_points: number of input grid points
-        - num_sensors: number of output grid points
+        - input_dim: number of input grid points
+        - output_dim: number of output grid points
         """
         super().__init__(params)
         self.L = 0.1 # in meters
         self.sigma = 0.01 # in meters
 
     def modality_name(self) -> str:
-        return "1d_blurring"
+        return "blur_1d"
 
     def setup_geometry(self) -> None:
-        self.x_in = np.linspace(0, self.L, self.params.num_brain_grid_points)
-        self.x_out = np.linspace(0, self.L, self.params.num_sensors)
-
-        # For compatibility with base class expectations
-        self.sources = self.x_in.reshape(-1, 1)  # Shape (N, 1)
-        self.sensors = self.x_out.reshape(-1, 1)  # Shape (N, 1)
+        self.x_in = np.linspace(0, self.L, self.params.input_dim)
+        self.x_out = np.linspace(0, self.L, self.params.output_dim)
 
     def compute_forward_model(self) -> np.ndarray:
-        """
-        Compute the Gaussian convolution matrix.
-
-        Returns
-        -------
-        np.ndarray
-            Convolution matrix K of shape (num_voxels, num_voxels)
-            where K[i,j] = exp(-((x_out[i] - x_in[j])^2) / (2*sigma^2))
-            normalized by dx / (sigma * sqrt(2*pi))
-        """
-        dx = self.L / self.params.num_brain_grid_points
+        dx = self.L / self.params.input_dim
 
         # Create convolution kernel matrix
-        K = np.zeros((self.params.num_sensors, self.params.num_brain_grid_points))
-        for i in range(self.params.num_sensors):
-            for j in range(self.params.num_brain_grid_points):
+        K = np.zeros((self.params.output_dim, self.params.input_dim))
+        for i in range(self.params.output_dim):
+            for j in range(self.params.input_dim):
                 # Gaussian kernel
                 K[i, j] = np.exp(-((self.x_out[i] - self.x_in[j]) ** 2) / (2 * self.sigma**2))
 
@@ -60,7 +46,7 @@ class Blur1D(ImagingModality):
         return K
     
     def _get_default_modality_params(self) -> Parameters:
-        return Parameters(num_brain_grid_points=128, num_sensors=128)
+        return Parameters(input_dim=128, output_dim=128)
 
 
 if __name__ == "__main__":
