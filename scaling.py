fbec07e517483e592d40a316d7a61bc3c9c83200
Lev <lc@lev.la>
2025-10-26 18:08:17 -0700

    blogpost

--- scaling.py
+++ scaling.py
@@ -9,9 +9,11 @@ from guti.scaling_utils import (
 )
 from guti.parameters import Parameters
 
-modality_name = "eeg_analytical"
+modality_name = "eeg_analytical_2"
 param_key = "num_brain_grid_points"
 constant_params = Parameters(num_sensors=110*110)
+# param_key = "num_sensors"
+# constant_params = Parameters(num_brain_grid_points=34*34*34)
 
 # %%
 plot_parameter_sweep_spectra(
