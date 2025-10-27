2f349747064c3b5eb14dea608f703d84c7cdbaf1
Raffi Hotter <raphael.hotter@gmail.com>
2025-10-27 00:35:20 +0000

    add show_sweep_results to param_sweep

--- param_sweep.py
+++ param_sweep.py
@@ -3,10 +3,11 @@ from guti.modalities.fnirs_analytical.modality import fNIRSAnalytical
 from guti.scaling_utils import show_sweep_results
 from copy import deepcopy
 
-param_name = "grid_resolution_mm"
-param_values = [5, 6, 8, 10]
+# param_name = "grid_resolution_mm"
+param_name = "num_sensors"
+param_values = [600, 800]
 
-default_params = Parameters(num_sensors=400)
+default_params = Parameters(grid_resolution_mm=6)
 
 for i, value in enumerate(param_values):
     print(f"-------------------------------- Running {param_name} = {value} ({i+1}/{len(param_values)}) --------------------------------")
@@ -16,8 +17,8 @@ for i, value in enumerate(param_values):
     modality.run(save_results=True)
 
 # After running the sweep, visualize the results:
-# show_sweep_results(
+show_sweep_results(
     modality_name=modality.name,
-#     param_key=param_name,
-#     constant_params=default_params
-# )
+    param_key=param_name,
+    constant_params=default_params
+)
